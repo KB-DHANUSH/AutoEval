b8235e49f80271477b5b3ec722e772b1c09e716f
KB-DHANUSH <kbdhanush1528@gmail.com>
2025-07-30 21:42:39 +0530

    created models.py

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from fastapi import File, UploadFile,Form
-from typing import List
+from typing import List,Annotated
 
 class ExamForm:
     def __init__(
@@ -13,4 +13,31 @@ class ExamForm:
         self.exam_questions = exam_questions
         self.rag_material = rag_material
         self.student_answers = student_answers
+
+class SubmitQuestionForm:
+    def __init__(
+        self,
+        exam_name: Annotated[str,Form(...)],
+        exam_questions: Annotated[UploadFile,File(...)]
+    ):
+        self.exam_name = exam_name
+        self.questions = exam_questions
+
+class SubmitAnswersForm:
+    def __init__(
+        self,
+        exam_name: Annotated[str,Form(...)],
+        student_answers: Annotated[List[UploadFile],File(...)]
+    ):
+        self.exam_name = exam_name
+        self.student_answers = student_answers
+
+class SubmitRagFileForm:
+    def __init__(
+        self,
+        exam_name: Annotated[str,Form(...)],
+        rag_material: UploadFile = File(...)
+    ):
+        self.exam_name = exam_name
+        self.rag_material = rag_material
     
