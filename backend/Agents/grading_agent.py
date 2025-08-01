fc4883efac8c2e89f025e0c9b226fd5813dfe11f
KB-DHANUSH <kbdhanush1528@gmail.com>
2025-08-01 16:15:57 +0530

    Edited grading_agent.py

--- backend/Agents/grading_agent.py
+++ backend/Agents/grading_agent.py
@@ -4,6 +4,7 @@ from pydantic_ai.common_tools.duckduckgo import duckduckgo_search_tool
 from Agents.models import GradingAgentDeps,GradingAgentOutput
 from httpx import AsyncClient
 from bson import ObjectId
+from typing import List
 from pymongo.database import Database
 from Agents.tools import rag_tool
 from Agents.prompts import GRADING_AGENT_PROMPT
@@ -12,7 +13,6 @@ from Agents.prompts import GRADING_AGENT_PROMPT
 
 class GradingAgent(Agent):
     def __init__(self, exam_name: str, user_id: ObjectId,db: Database):
-        self._agent: Agent
         self._agent_settings = {"temperature": 0.2}
         self._API_KEY = os.getenv("GROQ_API_KEY")
         self._deps = GradingAgentDeps(
@@ -22,18 +22,18 @@ class GradingAgent(Agent):
             user_id=str(user_id),
             db=db
         )
-    
-    async def grade(self,query: str):
-        self._default_retriesagent = Agent(
-            model='groq:mistral-saba-24b',
+        self._agent = Agent(
+            model='groq:moonshotai/kimi-k2-instruct',
             system_prompt=GRADING_AGENT_PROMPT,
             model_settings=self._agent_settings,
             deps_type=self._deps,
-            output_type=GradingAgentOutput,
+            output_type=List[GradingAgentOutput],
             tools=[
                 duckduckgo_search_tool(),
                 Tool(rag_tool,takes_ctx=True)
             ]
         )
+    
+    async def grade(self,query: str):
         response = await self._agent.run(query)
         return response.output
